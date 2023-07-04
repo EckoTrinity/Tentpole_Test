3835fed2658f0359c48ae9f19982747cec1bdc36
EckoTrinity <rishcar@gmail.com>
2023-07-04 22:08:56 +0200

    Add files via upload

---
+++ Tentpole/tentpole_test/tentpole_test/urls.py
@@ -0,0 +1,22 @@
+"""tentpole_test URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from question_1 import views as question_1_views
+
+urlpatterns = [
+    path('', question_1_views.form_name_view),
+    path('graph/', question_1_views.generate_graph, name='generate_graph'),
+]
