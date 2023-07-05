60f5f118eb4b4f2ddf7fddd805ea4623ca84f848
EckoTrinity <rishcar@gmail.com>
2023-07-05 13:11:58 +0200

    Delete Tentpole directory

--- Tentpole/tentpole_test/question_1/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-# Create a template for the form
-
-class FormName(forms.Form):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    date_of_birth = forms.DateField()
-    statement_file = forms.FileField()
